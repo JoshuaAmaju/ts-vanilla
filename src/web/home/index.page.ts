0e2522b08d91a5fa12ceafff24e01979e1a3b254
Joshua <princejosh0007@gmail.com>
2022-08-15 17:08:52 +0100

    Update index.page.ts

--- src/web/home/index.page.ts
+++ src/web/home/index.page.ts
@@ -6,6 +6,35 @@ enum Status {
 export default `
 <html>
 
+<head>
+<style>
+.loader {
+  --size: 2rem;
+  --thickness: 3px;
+  --color: var(--primary);
+  
+  border-radius: 50%;
+  width: var(--size);
+  height: var(--size);
+  border-style: solid;
+  border-color: var(--color);
+  border-top-color: transparent;
+  border-width: var(--thickness);
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
+</head>
+
 <body>
   <div>
     <p>Home page</p>
